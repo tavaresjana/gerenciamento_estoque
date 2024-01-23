4016534020c97beed986ea7540f62aa8a71e8f9f
Janaina Tavares <78567230+tavaresjana@users.noreply.github.com>
2024-01-23 11:42:01 -0300

    Primeiro commit: subindo projeto base

---
+++ src/test/java/com/gerenciamentoestoque/GerenciamentoestoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamentoestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoestoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
