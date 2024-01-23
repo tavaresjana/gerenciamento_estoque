530c7706d009af9f00d6b087ed6565942ce0d281
Janaina Tavares <78567230+tavaresjana@users.noreply.github.com>
2024-01-23 15:08:31 -0300

    feat: criação do método cadastrarProduto

--- src/main/java/com/gerenciamentoestoque/service/ProdutoService.java
+++ src/main/java/com/gerenciamentoestoque/service/ProdutoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
@@ -16,5 +17,9 @@ public class ProdutoService {
     public List<Produto> findAll(){
         return produtoRepository.findAll();
     }
+
+   public Produto cadastrarProduto(Produto produto){
+        return produtoRepository.save(produto);
+   }
 
 }
