59b6b73a1b9b9540eba674edd94d3367afc70355
Janaina Tavares <78567230+tavaresjana@users.noreply.github.com>
2024-04-04 16:11:30 -0300

    feat: adição do método de formatar cpf e cnpj e mudança no método atualizarProprietario

--- src/main/java/com/gerenciamentoestoque/service/ProprietarioService.java
+++ src/main/java/com/gerenciamentoestoque/service/ProprietarioService.java
@@ -20,24 +20,24 @@ public class ProprietarioService {
     @Autowired
     private ProprietarioMapper proprietarioMapper;
 
-    public List<ProprietarioDto> buscarTodosProprietarios(){
+    public List<ProprietarioDto> buscarTodosProprietarios() {
         List<Proprietario> listaProprietario = proprietarioRepository.findAll();
         List<ProprietarioDto> listaProprietarioDto = listaProprietario.stream().map(proprietarioMapper::entidadeParaDto).collect(Collectors.toList());
         return listaProprietarioDto;
     }
 
-    public ProprietarioDto buscarPorId(Long id){
+    public ProprietarioDto buscarPorId(Long id) {
         Optional<Proprietario> proprietario = proprietarioRepository.findById(id);
         Optional<ProprietarioDto> proprietarioDto = Optional.ofNullable(proprietarioMapper.entidadeParaDtoOp(proprietario));
         return proprietarioDto.get();
     }
 
-    public ProprietarioDto cadastrarProprietario(ProprietarioDto proprietarioDto){
+    public ProprietarioDto cadastrarProprietario(ProprietarioDto proprietarioDto) {
         return proprietarioMapper.entidadeParaDto(proprietarioRepository.save((proprietarioMapper.dtoParaEntidade(proprietarioDto))));
     }
 
-    public ProprietarioDto atualizarProprietario(Long id, ProprietarioDto proprietarioDto){
-        Optional<Proprietario> optionalProprietario = proprietarioRepository.findById(id);
+    public ProprietarioDto atualizarProprietario(ProprietarioDto proprietarioDto) {
+        Optional<Proprietario> optionalProprietario = proprietarioRepository.findById(proprietarioDto.getId());
 
         Proprietario proprietarioEditado = optionalProprietario.get();
 
@@ -49,21 +49,21 @@ public class ProprietarioService {
         return proprietarioMapper.entidadeParaDto(proprietarioEditado);
     }
 
-    public void desativarProprietario(Long id){
+    public void desativarProprietario(Long id) {
         Optional<Proprietario> optionalProprietario = proprietarioRepository.findById(id);
         Proprietario proprietarioEditado = optionalProprietario.get();
         proprietarioEditado.setAtivo(false);
         proprietarioRepository.save(proprietarioEditado);
     }
 
-    public void reativarProprietario(Long id){
+    public void reativarProprietario(Long id) {
         Optional<Proprietario> optionalProprietario = proprietarioRepository.findById(id);
         Proprietario proprietarioEditado = optionalProprietario.get();
         proprietarioEditado.setAtivo(true);
         proprietarioRepository.save(proprietarioEditado);
     }
 
-    public List<ProprietarioDto> buscarProprietariosAtivos(){
+    public List<ProprietarioDto> buscarProprietariosAtivos() {
         List<Proprietario> listaProprietarios = proprietarioRepository.findAll();
         List<Proprietario> listaProprietariosAtivos = listaProprietarios.stream()
                 .filter(Proprietario::getAtivo)
@@ -75,16 +75,32 @@ public class ProprietarioService {
         return listaProprietariosAtivosDto;
     }
 
-    public ProprietarioDto buscarPorCpf(String cpf){
-        Proprietario proprietario = proprietarioRepository.buscarPorCpf(cpf);
+    public ProprietarioDto buscarPorCpf(String cpf) {
+        Proprietario proprietario = proprietarioRepository.buscarPorCpf(formatarCPFouCNPJ(cpf));
         ProprietarioDto proprietarioDto = proprietarioMapper.entidadeParaDto(proprietario);
         return proprietarioDto;
     }
 
-    public ProprietarioDto buscarPorCnpj(String cnpj){
-        Proprietario proprietario = proprietarioRepository.buscarPorCnpj(cnpj);
+    public ProprietarioDto buscarPorCnpj(String cnpj) {
+        Proprietario proprietario = proprietarioRepository.buscarPorCnpj(formatarCPFouCNPJ(cnpj));
         ProprietarioDto proprietarioDto = proprietarioMapper.entidadeParaDto(proprietario);
         return proprietarioDto;
+    }
+
+    public static String formatarCPFouCNPJ(String valor) {
+        valor = valor.replaceAll("[^0-9]", "");
+        if (valor.length() == 11) {
+            return valor.substring(0, 3) + "." + valor.substring(3, 6) + "." +
+                    valor.substring(6, 9) + "-" + valor.substring(9);
+
+        } else if (valor.length() == 14) {
+            return valor.substring(0, 2) + "." + valor.substring(2, 5) + "." +
+                    valor.substring(5, 8) + "/" + valor.substring(8, 12) + "-" +
+                    valor.substring(12);
+        } else {
+            return null;
+            //add verificação de tamanho invalido p cpf
+        }
     }
 
 }
